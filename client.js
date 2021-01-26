a4125b7de836455cbfc9a4d25fad8e9eca0d5294
Vada Karlen <vada.karlen@gmail.com>
2021-01-26 14:41:38 -0600

    Added inital function to bonus calculator

--- client.js
+++ client.js
@@ -3,32 +3,32 @@ const employees = [
     name: 'Atticus',
     employeeNumber: '2405',
     annualSalary: '47000',
-    reviewRating: 3
+    reviewRating: 3,
   },
   {
     name: 'Jem',
     employeeNumber: '62347',
     annualSalary: '63500',
-    reviewRating: 4
+    reviewRating: 4,
   },
   {
     name: 'Scout',
     employeeNumber: '6243',
     annualSalary: '74750',
-    reviewRating: 5
+    reviewRating: 5,
   },
   {
     name: 'Robert',
     employeeNumber: '26835',
     annualSalary: '66000',
-    reviewRating: 1
+    reviewRating: 1,
   },
   {
     name: 'Mayella',
     employeeNumber: '89068',
     annualSalary: '35000',
-    reviewRating: 1
-  }
+    reviewRating: 1,
+  },
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -40,4 +40,23 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+// Write a declared function that takes in one Employee object (as an argument to the function), and returns a new object with the following properties:
+// The name property should contain the employee's name.
+// The bonusPercentage property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// The totalCompensation property should be the adjusted annual compensation (base annual + bonus)
+// The totalBonus should be the employee's total bonus rounded to the nearest dollar.
+
+for (i = 0; i < employees.length; i++) {
+  console.log(employees[i]);
+  console.log(employeeInfo(employees[i]));
+}
+
+function employeeInfo(employee) {
+  const newObject = {
+    name: employee.name, //checking the employ
+  };
+
+  return newObject;
+}
